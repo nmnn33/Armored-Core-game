d5c4e93ff3371c20b12c53bfd264f7aed24f2b78
nmnn33 <tommy123tran@gmail.com>
2023-12-29 22:33:46 +0200

    Main menu and Scene transitions
    descripted.

---
+++ scenes/mainMenu/main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_play_buttom_pressed():
+	get_tree().change_scene_to_file("res://scenes/mainScene/main_scene.tscn")
+
+func _on_options_buttom_2_pressed():
+	get_tree().change_scene_to_file("res://scenes/optionsMenu/optionsMenu.tscn")
+
+func _on_quit_buttom_3_pressed():
+	get_tree().quit()
