cbefe23371cb64dd5cfdb8135138d0ebd9fc4873
nmnn33 <tommy123tran@gmail.com>
2024-01-02 22:38:09 +0200

    updates 121
    Player node is now characterbody2d, better scripting.
    Fixing code here and there.
    Tileset coloring and collisions.

--- scenes/enemies/enemy1/slime.gd
+++ scenes/enemies/enemy1/slime.gd
@@ -7,4 +7,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$AnimatedSprite2D.play("run")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,8 +1,6 @@
-extends Area2D
+extends CharacterBody2D
 
-signal hit
-
-@export var speed = 400
+var speed = 400
 var screen_size
 
 #This always activates first
@@ -11,33 +9,19 @@ func _ready():
 	# Set the remote path to the direct child Camera2D node in the main scene
 	$RemoteTransform2D.remote_path = get_parent().get_node("Camera2D").get_path()
 
-#Constant things
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+func get_input():
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_dir * speed
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
 		#plays the idle animation as default
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.play("idle")
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
 	#Checks if you run left or right and flips horizontally accordingly
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "run"
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 
-#When you touch other objects, you take damage and become invincible for 2 sec (later)
-func _on_body_entered(body):
-	hide()
-	hit.emit()
-	$CollisionShape2D.set_deferred("disabled", true)
-	
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
